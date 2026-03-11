85d6a950d4c43b1bfe424c39f91f4b8935c069fa
Wen-diti <contaparatrabalhos2102@gmail.com>
2026-03-11 11:38:19 -0300

    aula 07

---
+++ aula_7/exercicio2/script.js
@@ -0,0 +1,24 @@
+let users = []
+let posts = []
+
+async function fetchData() {
+    const userData = await fetch('https://jsonplaceholder.typicode.com/users')
+    const postsData = await fetch('https://jsonplaceholder.typicode.com/posts')
+    users = await userData.json()
+    posts = await postsData.json()
+}
+
+async function setData(){
+    await fetchData()
+
+    const usersWithPosts = users.map(user => {
+        return {
+            ...user,
+            posts: posts.filter(post => post.userId === user.id)
+        }
+    })
+
+    console.log(usersWithPosts)
+}
+
+setData()
