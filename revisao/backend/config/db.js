a141efdf1b32a4c61c27d60dc6605ee4f149f8c4
Wen-diti <contaparatrabalhos2102@gmail.com>
2026-05-27 11:32:06 -0300

    conexao bd

--- revisao/backend/config/db.js
+++ revisao/backend/config/db.js
@@ -1,12 +1,15 @@
 import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const connection = mysql.createConnection({
-    HOST: process.env.HOST,
-    USER: process.env.USER,
-    PASSWORD: process.env.PASSWORD,
-    DATABASE: process.env.DATABASE,
-    PORT: process.env.PORT
-});
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORT
+})
 
 connection.connect((err) => {
     if (err) {
--- revisao/backend/src/controllers/controllerProduto.js
+++ revisao/backend/src/controllers/controllerProduto.js
@@ -1,16 +1,26 @@
 import mysql, { Connection } from 'mysql2';
-import dotenv from 'doten';
+import dotenv from 'dotenv';
 import connection from '../../config/db.js'
 
 export const getProdutos = (req, res) => {
-    return res.status(200)
-    return 'select * from produto'
+    try {
+        connection.query('select * from produto', (error, results) => {
+            if (error) {
+                return res.status(500).json({error: error.message});
+            }
+            return res.status(201).json({message: 'Lista dos produtos', produto: results})
+        })
+    }
+
+    catch (error) {
+        return res.status(400).json({message: 'Erro ao puxar Lista de produtos'})
+    }
 }
 
 export const criarProduto = (req, res) => {
     try {
-        const {nome, preco, quantidade} = req.body;
-        connection.query('Insert into produto(nome, valor, quantidade) values (?, ?, ?)', [nome, preco, quantidade], (error, results) => {
+        const {nome, valor, quantidade} = req.body;
+        connection.query('Insert into produto(nome, valor, quantidade) values (?, ?, ?)', [nome, valor, quantidade], (error, results) => {
             if (error) {
                 return res.status(500).json({error: error.message});
             }
